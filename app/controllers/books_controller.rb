5504c14cc223afeaf776b8c2044f1827ae96ef17
EC2 Default User <ec2-user@ip-172-31-32-245.ap-northeast-1.compute.internal>
2025-09-18 11:25:51 +0000

    [Finish] books/indexページ左側

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,26 @@
 class BooksController < ApplicationController
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(books_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   def index
+    @books = Book.all
+    @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @book.id = current_user.id
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  
-  def create
-  end
-
-  def edit
-  end
 
   def index
+    @users = User.all
   end
 
   def show
+    @User = User.find(params[:id])
+  end
+
+  def edit
+    @User = User.find(params[:id])
   end
 end
