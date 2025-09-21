377332958f53c65ca16fd6717131190ea0ffb9f7
EC2 Default User <ec2-user@ip-172-31-32-245.ap-northeast-1.compute.internal>
2025-09-21 06:25:47 +0000

    [Finish] バグ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_one_attached :image
 
   # 一意性を持たせ、かつ2~20文字以内
-  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
   # 最大50文字まで
   validates :introduction, length: { maximum: 50}
 end
